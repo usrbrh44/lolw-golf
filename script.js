b393a6268b3868ca3adb9c91adba4b151a4e20a4
usrbrh44 <bradhetrick@gmail.com>
2025-07-21 22:12:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+console.log("Script starting to load...");
 // Import Firebase
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js';
 import { getFirestore, doc, onSnapshot, setDoc } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js';
